2d14d86cacfa8825695e352761467e9397b21493
Leonardo José da Cunha Peixoto <sphommer89@>
2020-08-05 17:17:26 -0300

    Implementar e usar PageDefault, com Menu no cabeçalho e Footer embaixo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Menu from '../../components/Menu';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
+import PageDefault from '../../components/PageDefault';
 import dadosIniciais from '../../data/dados_iniciais.json';
 
 function renderCategoria(categoria) {
@@ -14,20 +14,22 @@ function Home() {
   return (
     <div style={{ background: "#141414"}}>
 
-        <Menu />
+        <PageDefault>
 
-        <BannerMain
-            videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-            url={dadosIniciais.categorias[0].videos[0].url}
-            videoDescription={"O que é Front-end ? Trabalhando na área"}
-        />
+            <BannerMain
+                videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+                url={dadosIniciais.categorias[0].videos[0].url}
+                videoDescription={"O que é Front-end ? Trabalhando na área"}
+            />
 
-        <Carousel
-            ignoreFirstVideo
-            category={ dadosIniciais.categorias[0] }
-        />
+            <Carousel
+                ignoreFirstVideo
+                category={ dadosIniciais.categorias[0] }
+            />
 
-        { dadosIniciais.categorias.slice(1).map(renderCategoria) }
+            { dadosIniciais.categorias.slice(1).map(renderCategoria) }
+
+        </PageDefault>
 
     </div>
   );
