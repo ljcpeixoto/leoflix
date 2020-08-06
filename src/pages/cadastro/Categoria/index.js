5e12077127a760cd7182f0112df7512074630dbc
Leonardo José da Cunha Peixoto <sphommer89@>
2020-08-06 12:32:42 -0300

    Adicionar funcionalidade no form do Cadastro de Categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,26 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+    const valorInicial = {
+        nome: '',
+        descricao: '',
+        cor: '#000000'
+    }
+
+    const [categorias, setCategorias] = useState([]);
+    const [novaCategoria, setNovaCategoria] = useState(valorInicial);
+
+    function setValor(chave, valor) {
+        setNovaCategoria({
+            ...novaCategoria,
+            [chave]: valor
+        })
+    }
+
+    function onChangeValor(info) {
+        setValor(
+            info.target.getAttribute('name'),
+            info.target.value);
+    }
+
     return (
         <PageDefault>
-            <h1>Página de Cadastro de Categoria </h1>
+            <h1>Página de Cadastro de Categoria</h1>
 
-            <form>
+            <form onSubmit={function handleSubmit(infoDoForm) {
+                infoDoForm.preventDefault();
+                console.log('Tentou enviar form: ', infoDoForm);
+                setCategorias([...categorias, novaCategoria])
+            }}>
 
-                <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
-                    />
-                </label>
+                <div>
+                    <label>
+                        Nome da Categoria:
+                        <input
+                            type="text"
+                            name="nome"
+                            value={novaCategoria.nome}
+                            onChange={onChangeValor}
+                        />
+                    </label>
+
+                    <label>
+                        Descrição:
+                        <textarea
+                            type="text"
+                            name="descricao"
+                            value={novaCategoria.descricao}
+                            onChange={onChangeValor}
+                        />
+                    </label>
+
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            name="cor"
+                            value={novaCategoria.cor}
+                            onChange={onChangeValor}
+                        />
+                    </label>
+
+                </div>
 
                 <button>
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                        return <li key={categoria + indice}>
+                            {categoria.nome}
+                        </li>
+                    })
+                }
+            </ul>
+
 
             <Link to="/">
                 Ir para Home
